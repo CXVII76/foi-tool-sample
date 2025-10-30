ad8d891d3fe5d57cab60bf0174a7f98ec990162c
CXVII76 <mayxu57@yahoo.com>
2025-10-30 19:43:07 +1100

    chore: update button text on branch

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
           style={{ padding: '10px 20px', fontSize: '16px', marginTop: '20px' }}
           onClick={() => alert('GitHub Flow works!')}
         >
-          Click Me on Main
+          Press Here on Branch
         </button>
       </header>
     </div>
