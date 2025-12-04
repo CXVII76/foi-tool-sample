6403c2c61256f201e8ea3cf47599c234150b496b
CXVII76 <mayxu57@yahoo.com>
2025-12-04 15:09:25 +1100

    fix: add react-refresh plugin for Vite Fast Refresh

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,17 +6,18 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
+    'plugin:react-refresh/recommended',   // ← this line fixes the error
     'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json' },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'react-refresh'],  // ← add react-refresh here
   rules: {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
-    "react-refresh/only-export-components": "warn",
-    'no-console': 'warn'
+    'no-console': 'warn',
+    'react-refresh/only-export-components': 'warn'  // ← optional: allow non-components if needed
   },
   settings: { react: { version: 'detect' } }
 };
